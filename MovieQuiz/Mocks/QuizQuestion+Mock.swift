d7600274bf5aa442cc90a946245dbb2b048ce04f
wialux12 <kirill.spynu@icloud.com>
2025-04-30 20:40:56 +0500

    Проект отредактирован

---
+++ MovieQuiz/Mocks/QuizQuestion+Mock.swift
@@ -0,0 +1,7 @@
+//
+//  Mocks.swift
+//  MovieQuiz
+//
+//  Created by Кирилл Спыну on 30.04.2025.
+//
+
---
+++ MovieQuiz/Presentation/ViewModel.swift
@@ -0,0 +1,25 @@
+ struct ViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    // для состояния "Вопрос показан"
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    // для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+    
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
